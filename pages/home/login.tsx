f26c562986baae8ccefa918b7d84727af7425614
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2023-05-29 19:09:16 -0300

    fim 6

---
+++ pages/home/login.tsx
@@ -0,0 +1,172 @@
+import type { ReactElement } from "react";
+import { useSession, getCsrfToken, signIn } from "next-auth/react";
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import toast from "react-hot-toast";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Link from "next/link";
+
+import type { NextPageWithLayout } from "types";
+import { AuthLayout } from "@/components/layouts";
+import { InputWithLabel } from "@/components/ui";
+import { getParsedCookie } from "@/lib/cookie";
+import env from "@/lib/env";
+
+import { HStack, Stack, Text } from "@chakra-ui/react"
+
+import { ButtonLogin } from "@/components/Buttons"
+import { FormLogin } from "@/components/Forms"
+import { Links } from "@/components/Links"
+import { useToast } from '@chakra-ui/react'
+
+const Login: NextPageWithLayout<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ csrfToken, redirectAfterSignIn }) => {
+  const { status } = useSession();
+  const router = useRouter();
+  const { t } = useTranslation("common");
+
+  if (status === "authenticated") {
+    router.push(redirectAfterSignIn);
+  }
+
+  const toast = useToast()
+  const statuses = ['success', 'error', 'warning', 'info']
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().required().email(),
+      password: Yup.string().required(),
+    }),
+    onSubmit: async (values) => {
+      const { email, password } = values;
+
+      const response = await signIn("credentials", {
+        email,
+        password,
+        csrfToken,
+        redirect: false,
+        callbackUrl: redirectAfterSignIn,
+      });
+
+      formik.resetForm();
+
+      if (!response?.ok) {
+        {
+
+          toast({
+            title: `Email ou senha incorretos`,
+            status: 'error',
+            isClosable: true,
+          })
+
+        }
+        return;
+      }
+
+    },
+  });
+
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <HStack
+          as="main"
+          bgImage="url('/login.png')"
+          justify={"end"}
+        >
+          <Stack
+            as="article"
+            spacing={8}
+            bg={"#fbfff2b5"}
+            borderTopLeftRadius={"full"}
+            borderBottomLeftRadius={"full"}
+            px={24}
+            py={72}
+          >
+            <Stack
+              spacing={6}
+            >
+              <FormLogin placeholder={"Email"} type="email" onChange={formik.handleChange} name="email" value={formik.values.email} />
+              {/* <InputWithLabel
+                type="email"
+                label="Email"
+                name="email"
+                placeholder="Email"
+                value={formik.values.email}
+                error={formik.touched.email ? formik.errors.email : undefined}
+                onChange={formik.handleChange}
+              /> */}
+              <FormLogin placeholder={"Senha"} type="password" onChange={formik.handleChange} name="password" value={formik.values.password} />
+              {/* <InputWithLabel
+                type="password"
+                label="Password"
+                name="password"
+                placeholder="Password"
+                value={formik.values.password}
+                error={
+                  formik.touched.password ? formik.errors.password : undefined
+                }
+                onChange={formik.handleChange}
+              /> */}
+
+
+              <Links
+                href={"/login/esqueci-a-senha"}
+              >
+                <Text
+                  as="p"
+                  color={"#2FAFCA"}
+                  fontSize={"16px"}
+                  fontWeight={400}
+                >
+                  Esqueci a senha
+                </Text>
+              </Links>
+
+
+            </Stack>
+            <ButtonLogin text={"Entrar"} type="submit" />
+          </Stack>
+        </HStack>
+      </form>
+
+    </>
+  );
+};
+
+Login.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <AuthLayout heading="Welcome back" description="Log in to your account">
+      {page}
+    </AuthLayout>
+  );
+};
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const { req, res, locale }: GetServerSidePropsContext = context;
+
+  const cookieParsed = getParsedCookie(req, res);
+
+  return {
+    props: {
+      ...(locale ? await serverSideTranslations(locale, ["common"]) : {}) || null,
+      csrfToken: await getCsrfToken(context) || null,
+      redirectAfterSignIn: cookieParsed.url ?? env.redirectAfterSignIn,
+    },
+  };
+};
+
+export default Login;
