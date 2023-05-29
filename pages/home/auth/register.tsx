ab2bd0b2dd977dad5f060ae0a8344a579243adf0
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2023-05-29 19:12:00 -0300

    fim 8

---
+++ pages/home/auth/register.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+export default function Register() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault()
+
+        try {
+            const response = await fetch('/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password, name }),
+            })
+
+            if (!response.ok) {
+                const error = await response.text()
+                throw new Error(error)
+            }
+
+            const user = await response.json()
+            console.log(user)
+        } catch (error) {
+            //@ts-ignore
+            setError(error.message)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Name:
+                <input type="name" value={name} onChange={(e) => setName(e.target.value)} />
+            </label>
+
+            <label>
+                Email:
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+
+            <label>
+                Password:
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+
+            <button type="submit">Register</button>
+
+            {error && <p>{error}</p>}
+        </form>
+    )
+}
