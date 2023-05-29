c720695b249a95e3b56e4d0948f997bbce8fe7fa
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2023-05-28 21:56:39 -0300

    finalizado v5

--- middleware.ts
+++ middleware.ts
@@ -18,14 +18,14 @@ export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers.get('host') || 'demo.vercel.pub';
+  const hostname = req.headers.get('host') || 'denload-sistema.vercel.pub';
 
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
 
   // Only for demo purposes - remove this if you want to use your root domain as the landing page
   if (hostname === 'vercel.pub' || hostname === 'platforms.vercel.app') {
-    return NextResponse.redirect('https://demo.vercel.pub');
+    return NextResponse.redirect('https://denload-sistema.vercel.pub');
   }
 
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
